1edd7bf6bc6eb8dae63267d69c3fc1cc73acf427
Samanyu03 <msamanyu21@gmail.com>
2025-10-23 20:30:23 +0530

    Add files via upload

---
+++ virus scannner.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+
+root=Tk()
+root.geometry("200x200")
+def msg():
+    messagebox.showwarning("alert","virus found!!!")
+    
+button=Button(root,text="scan for virus",command=msg)
+button.place(x=80,y=50)
+root.mainloop()
